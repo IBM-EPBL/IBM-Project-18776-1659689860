f9e5ce4c7c75a349fca1b15954c2bbe286360756
Ramasubramaniam Gv <gvpranav747@gmail.com>
2022-11-17 18:30:18 +0530

    deleted

--- Assignments/Team Member 3/Assignment 1/post.php
+++
@@ -1,7 +0,0 @@
-<?php
-   $name= $_POST["name"];
-   $email= $_POST["email"];
-    $Number = $_POST["Number"];
-    echo "Ny name is $name<br/> My mail id is $email<br/> My number is {$Number}"
-
-?>
